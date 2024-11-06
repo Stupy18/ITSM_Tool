9834bbfb74a66eb0c90cfe9003aeca9de2549211
Tenche Eduard <tencheeduard@gmail.com>
2024-11-06 05:21:56 +0200

    temp fix for conflicting config files

--- ubb/ubb/src/main/java/ubb/project/ubb/config/WebConfig.java
+++ ubb/ubb/src/main/java/ubb/project/ubb/config/WebConfig.java
@@ -1,5 +1,6 @@
 package ubb.project.ubb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,5 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
                 allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").
                 allowedHeaders("*").
                 allowCredentials(true);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
