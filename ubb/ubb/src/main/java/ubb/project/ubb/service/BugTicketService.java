33a2664f013ff7effd10349e87b2ff5df36ee338
Mihai Salcudean <mitzu.v99444@yahoo.com>
2024-12-09 00:34:18 +0200

    Added backend too and sum lil fixes <3

--- ubb/ubb/src/main/java/ubb/project/ubb/service/BugTicketService.java
+++ ubb/ubb/src/main/java/ubb/project/ubb/service/BugTicketService.java
@@ -2,8 +2,10 @@ package ubb.project.ubb.service;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ubb.project.ubb.data.BugTicket;
+import ubb.project.ubb.dto.AddableTicketDto;
 import ubb.project.ubb.dto.BugTicketDto;
 import ubb.project.ubb.mapper.BugTicketMapper;
 import ubb.project.ubb.repository.IBugTicketRepository;
@@ -23,5 +25,11 @@ public class BugTicketService {
 
     public List<BugTicketDto> getByProject(Long id){
         return bugTicketRepository.findAllByProject_Id(id).stream().map((BugTicket ticket)->{return bugTicketMapper.entityToDto(ticket);}).toList();
+    }
+
+    public BugTicket addTicket(AddableTicketDto ticket) {
+        BugTicket entity = bugTicketMapper.addableDtoToEntity(ticket);
+        bugTicketRepository.save(entity);
+        return entity;
     }
 }
