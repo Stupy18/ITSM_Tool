89d66f640388f93c6d61659aa1c96f4b8c048973
mircea <mircea.rautoiu@gmail.com>
2024-12-09 20:16:42 +0200

    Refactoring

--- ubb/ubb/src/main/java/ubb/project/ubb/repository/IFileRepository.java
+++ ubb/ubb/src/main/java/ubb/project/ubb/repository/IFileRepository.java
@@ -3,5 +3,8 @@ package ubb.project.ubb.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ubb.project.ubb.data.File;
 
+import java.util.List;
+
 public interface IFileRepository extends JpaRepository<File, Long> {
+    List<File> findByProjectId(Long projectId);
 }
