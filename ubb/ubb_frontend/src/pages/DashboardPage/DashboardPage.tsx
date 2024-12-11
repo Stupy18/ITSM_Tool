79ed61f64c8012ff7c85ed259bc1415166234b26
mircea <mircea.rautoiu@gmail.com>
2024-12-11 16:26:49 +0200

    Remove some useless stuff

--- ubb/ubb_frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ ubb/ubb_frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./DashboardPage.css";
 import FileUpload from "../DashboardPage/Components/FileUpload.tsx";
 import { LocalStorageEnum } from "../../enum/LocalStorageEnum.tsx";
@@ -7,7 +6,6 @@ import FileList from "../DashboardPage/Components/FileList.tsx";
 
 export default function DashboardPage() {
   const [username, setUsername] = useState<string>(" ");
-  const navigate = useNavigate();
 
   useEffect(() => {
     const storedUsername = localStorage.getItem(LocalStorageEnum.USER_NAME);
@@ -16,17 +14,9 @@ export default function DashboardPage() {
     }
   }, []);
 
-  const handleLogout = () => {
-    localStorage.removeItem(LocalStorageEnum.USER_NAME);
-    navigate("/login");
-  };
-
   return (
     <div className="dashboard-page">
       <h1>Welcome to the Dashboard, {username}!</h1>
-      <button onClick={handleLogout} className="logout-button">
-        Logout
-      </button>
       <div className="upload-section">
         <h2>Upload Files</h2>
         <FileUpload />
